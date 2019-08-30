af1f06395bb27bea809ab11a18358d5dc5e70f92
BrunoUnias <37314205+BrunoUnias@users.noreply.github.com>
2019-08-30 20:03:11 -0300

    Add files via upload

---
+++ main2.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+typedef struct ElementoLista{
+	char *dado;
+	struct ElementoLista *seguinte;
+	
+}Elemento;
+
+typedef struct ListaDetectada{
+	Elemento *inicio;
+	int tamanho;
+	
+}Pilha;
+
+void inicialização(Pilha *monte){
+	monte->inicio = NULL;
+	monte->tamanho = 0;
+	
+}
+
+int empilhar(Pilha *monte, char *dado){
+	Elemento *novo_elemento;
+	if((novo_elemento = (Elemento *)malloc(sizeof(Elemento))) == NULL)
+	return -1;
+	if((novo_elemento->dado = (char *) malloc(50 *sizeof(char))) == NULL)
+	return -1;
+	
+	strcopy(novo_elemento->dado,dado);
+	novo_elemento->seguinte = monte->inicio;
+	monte->inicio = novo_elemento;
+	monte->tamanho++;
+	
+}
+
+int desempilhar(Pilha *monte){
+	Elemento remove_elemento;
+	if(monte->tamanho == 0)
+	return -1;
+	remove_elemento = monte->inicio;
+	monte->inicio = monte->inicio->seguinte;
+	free(remove_elemento->dado);
+	free(remove_elemento);
+	monte->tamanho--;
+	return 0;
+	
+}
+
+void exibirPilha(Pilha *monte){
+	Elemento *corrente;
+	int i;
+	Pilha *curso = monte->inicio;
+	for(i=0;i<tas->tamanho;++i){
+printf("\t\t%s\n", corrente->dado);
+ curso = em curso->próximo;
+}
+}
