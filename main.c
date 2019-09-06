80f6bba23e09549cc3dbb70e1d6cca221b19d864
BrunoUnias <37314205+BrunoUnias@users.noreply.github.com>
2019-09-06 19:44:08 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,54 +1,80 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
-typedef struct ElementoLista{
-	char *dado;
-	struct ElementoLista *seguinte;
+typedef struct ElementoPilha{
+	int *dado;
+	struct Pilha *seguinte;
 	
 }Elemento;
 
-typedef struct ListaDetectada{
+typedef struct Lista{
 	Elemento *inicio;
 	int tamanho;
 	
 }Pilha;
 
-void inicialização(Pilha *monte){
+void inicializar(Pilha *monte){
 	monte->inicio = NULL;
 	monte->tamanho = 0;
 	
 }
 
-int empilhar(Pilha *monte,char *dado){
-	Elemento *novo_elemento;
-	if((novo_elemento = (Elemento *) malloc(sizeof(Elemento))) == NULL)
+int empilhar(Pilha *monte,int *inicio){
+	Elemento *novo;
+	if((novo = (Elemento *) malloc(sizeof(Elemento))) == NULL)
 	return -1;
-	if((novo_elemento->dado = (char *) malloc(50 *sizeof(char))) == NULL)
+	if((novo->dado = (int *) malloc(50 *sizeof(int))) == NULL)
 	return -1;
 	
-	strcpy(novo_elemento->dado,dado);
-	novo_elemento->proximo = monte->inicio;
-	monte->inicio = novo_elemento;
+	monte->inicio = novo;
+	novo->seguinte = monte->inicio;
+	monte->inicio = novo;
 	monte->tamanho++;
 	
 }
 
 int desempilhar(Pilha *monte){
-	Elemento *remov_elemento;
+	Elemento *remove_elemento;
 	if(monte->tamanho == 0)
 	return -1;
-	remov_elemento = monte->inicio;
-	tas->inicio = tas->inicio->proximo;
-	free(remov_elemento->dado);
-	free(remov_elemento);
+	remove_elemento = monte->inicio;
+	monte->inicio = monte->inicio->seguinte;
+	//free(remove_elemento->dado);
+	free(remove_elemento);
 	monte->tamanho--;
 	return 0;
 	
 }
 
+void exibirPilha(Pilha *monte){
+	Elemento *corrente;
+	int i;
+	Pilha *curso = monte->inicio;
+	for(i = 0; i<curso->tamanho;i++){
+		printf("\t\t%s\n",corrente->dado);
+		curso = corrente->seguinte;
+		
+	}
+}
 
-int main(int argc, char *argv[]) {
+int main() {
+	int opc,a;
+	Pilha *i;
+	
+	printf("Escolha a opcao.");
+	printf("1-empilhar\n2-desempilhar\n3-exibir\n4-sair");
+	scanf("%d",&opc);
+	switch(opc){
+		case 1:
+			empilhar(i,a);
+			break;
+		case 2:
+			desempilhar(i);
+			break;
+		case 3:
+			exibirPilha(i);
+			break;
+	}
 	return 0;
 }
